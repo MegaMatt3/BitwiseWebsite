8a9f4e00119acdde9948ecff20a6d87a86bcc0de
MegaMatt3 <vctn57gr>
2019-02-15 20:59:56 -0500

    Updated logo

--- src/js/home.js
+++ src/js/home.js
@@ -1,20 +1,4 @@
 window.onload = function() {
-  const coloredItems = document.body.querySelectorAll('#content .colored');
-  const colors = [
-    '#1b75bc',
-    '#bc441b',
-    '#901bbc',
-    '#1bbc84'
-  ];
-
-  coloredItems.forEach((el) => {
-    el.style.color = colors[randomIntFromInterval(0, colors.length-1)];
-  });
-
   // fade in elements
   document.getElementById('content').classList.add('show');
 };
-
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
